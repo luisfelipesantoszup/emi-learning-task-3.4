c7667520d63a06764edc6ba826cf2c7e55bb88a0
Luis Felipe dos Reis Santos <luis.felipe@zup.com.br>
2022-10-17 18:40:48 -0300

    tarefa finalizada com implementação de validação básica de campo em branco

--- LearningTask-3-4/LearningTask-3-4/ViewController.swift
+++ LearningTask-3-4/LearningTask-3-4/ViewController.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var senhaTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func botaoLoginPressionado(_ sender: UIButton) {
+        if emailTextField.text == "" || senhaTextField.text == "" {
+            exibirAlertaDeCampoEmBranco()
+        }
+        else {
+            exibirAlertaDeLogin()
+        }
+    }
     
+    func exibirAlertaDeLogin() {
+        let titulo = "Logando"
+        let mensagem = "Fazendo login na sua conta..."
+        
+        let alert = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func exibirAlertaDeCampoEmBranco() {
+        let titulo = "Favor preencher os campos."
+        let mensagem = "O campo Email ou Senha estão em branco, verifique suas credenciais."
+        
+        let alert = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func botaoRedefinirPressionado(_ sender: UIButton) {
+        exibirAlertaRedefinirSenha()
+    }
+    
+    func exibirAlertaRedefinirSenha() {
+        let titulo = "Redefinir senha"
+        let mensagem = "Enviamos um email para \(emailTextField.text!).\nSiga as instruções para criar uma nova senha segura."
+        
+        let alert = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
